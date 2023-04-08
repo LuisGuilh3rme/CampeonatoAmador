82b82b645052279f6adf69a34122cef5fba070f1
Luis Guilherme <luislebert01@outlook.com>
2023-04-08 20:39:18 -0300

    Trigger e procedure para alteração de pontuação na classificação adicionados

--- CampeonatoAmador.sql
+++ CampeonatoAmador.sql
@@ -3,6 +3,7 @@ GO
 USE CampeonatoAmador
 GO
 
+-- Tabelas
 CREATE TABLE [Time] (
     [Nome] VARCHAR(50) NOT NULL,
     [Apelido] VARCHAR(30) NOT NULL,
@@ -10,6 +11,7 @@ CREATE TABLE [Time] (
 
     CONSTRAINT PK_Time PRIMARY KEY ([Nome])
 )
+GO
 
 CREATE TABLE [Classificacao] (
     [Nome_Time] VARCHAR(50) NOT NULL,
@@ -18,12 +20,12 @@ CREATE TABLE [Classificacao] (
     [Total_Gols_Sofridos] INT NOT NULL DEFAULT (0),
     [Saldo_Gols] INT NOT NULL DEFAULT(0),
     [Vitorias] INT NOT NULL DEFAULT (0),
-    [Derrotas] INT NOT NULL DEFAULT(0),
     [Empates] INT NOT NULL DEFAULT(0),
     
     CONSTRAINT PK_Classificacao PRIMARY KEY ([Nome_Time]),
     CONSTRAINT FK_Classificacao_Time FOREIGN KEY ([Nome_Time]) REFERENCES [Time] ([Nome])
 )
+GO
 
 CREATE TABLE [Partida] (
     [Time_Casa] VARCHAR(50) NOT NULL,
@@ -35,3 +37,42 @@ CREATE TABLE [Partida] (
     CONSTRAINT FK_Partida_Time_Casa FOREIGN KEY ([Time_Casa]) REFERENCES [Time]([Nome]),
     CONSTRAINT FK_Partida_Time_Visitante FOREIGN KEY ([Time_Visitante]) REFERENCES [Time]([Nome])
 )
+GO
+
+-- Triggers
+CREATE TRIGGER TGR_Partida_Insert ON [Partida] AFTER INSERT AS 
+    BEGIN
+        DECLARE @Casa VARCHAR(50), @Visitante VARCHAR(50), @Gols_Casa INT, @Gols_Visitante INT
+
+        SELECT @Casa = [Time_Casa], @Visitante = [Time_Visitante], @Gols_Casa = [Gols], @Gols_Visitante = [Gols_Sofridos] FROM INSERTED
+
+        EXEC.Alterar_Classificacao @Casa, @Gols_Casa, @Gols_Visitante, 'Casa'
+        EXEC.Alterar_Classificacao @Visitante, @Gols_Visitante, @Gols_Casa, 'Visitante'
+    END
+GO
+
+-- Procedures
+CREATE OR ALTER PROC Alterar_Classificacao @Time VARCHAR(50), @Gols INT, @Gols_Sofridos INT, @Lado VARCHAR(9) AS
+    BEGIN
+        DECLARE @Pontuacao INT
+
+        -- Contabilizando gols
+        UPDATE [Classificacao] SET [Total_Gols] = [Total_Gols] + @Gols, [Total_Gols_Sofridos] = [Total_Gols_Sofridos] + @Gols_Sofridos, [Saldo_Gols] = [Saldo_Gols] + (@Gols - @Gols_Sofridos) WHERE [Nome_Time] = @Time
+
+        -- Verificando empate ou vitoria
+        IF (@Gols > @Gols_Sofridos)
+            UPDATE [Classificacao] SET [Vitorias] = [Vitorias] + 1 WHERE [Nome_Time] = @Time
+        IF (@Gols = @Gols_Sofridos)
+            UPDATE [Classificacao] SET [Empates] = [Empates] + 1 WHERE [Nome_Time] = @Time
+
+        -- Calculando Pontuacao
+        SET @Pontuacao = CASE 
+            WHEN @Gols = @Gols_Sofridos THEN 1
+            WHEN @Gols > @Gols_Sofridos AND @Lado = 'Casa' THEN 3
+            WHEN @Gols > @Gols_Sofridos AND @Lado = 'Visitante' THEN 5
+            ELSE 0
+        END
+
+        UPDATE [Classificacao] SET [Pontuacao] = [Pontuacao] + @Pontuacao WHERE [Nome_Time] = @Time
+    END
+GO
